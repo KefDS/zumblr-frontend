d3e9d874c264a845dc70a2151a65fb94fd651d6e
Kevin Delgado Sandí <kefdelgado@gmail.com>
2017-06-18 18:14:37 -0600

    feat: navbar :-(

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import navbarIcons from './navbarIcons'
+
+import './bootstrap-navbar.css'
 import './navbar.css'
 
 const SiteNavbar = () => (
-  <nav className='navbar navbar-default navbar-fixed-top navbar-custom'>
+  <nav className='navbar navbar-default navbar-fixed-top'>
     <div className='container-fluid'>
 
       <div className='navbar-header'>
@@ -14,24 +17,31 @@ const SiteNavbar = () => (
           <span className='icon-bar' />
           <span className='icon-bar' />
         </button>
-        <a className='navbar-brand' href='#'><img src='/assets/img/tumblr_logo.png' alt='tumblr' /></a>
+        <Link to='/' className='navbar-brand'><img className='navbar__logo' src='/assets/img/tumblr_logo.png' alt='tumblr' /></Link>
       </div>
 
-      <div className='collapse navbar-collapse' id='menu'>
+      <div id='menu' className='collapse navbar-collapse'>
 
-        <form className='navbar-form navbar-left'>
-          <div className='form-group'>
-            <input type='text' className='form-control' placeholder='Search' />
-          </div>
-          <button type='submit' className='btn btn-default'>Submit</button>
+        <form className='navbar-form navbar-left navbar__search-box'>
+          {/* TODO: Find icon */}
+          <button type='submit' className='navbar__search-btn'>:v</button>
+          <input type='text' className='form-control navbar__search-input' placeholder='Search Tumblr' />
         </form>
 
-        <ul className='nav navbar-nav navbar-right'>
-          <li><a href='#'>Link</a></li>
+        <ul className='nav navbar-nav navbar-right navbar__right-icons'>
+          {navbarIcons.map((navbarIcon, index) =>
+            (<li key={index}>
+              <NavbarIcon route='#' img={navbarIcon.path} alt={navbarIcon.alt} />
+            </li>)
+          )}
         </ul>
       </div>
     </div>
   </nav>
+)
+
+const NavbarIcon = ({ route, img, alt }) => (
+  <Link to={route}><img className='svg navbar__icon' src={img} /></Link>
 )
 
 export default SiteNavbar

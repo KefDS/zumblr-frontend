3bb6ad5bdb7b78478691a7bee390fd6da6199962
Kevin Delgado <kefdelgado@gmail.com>
2017-06-21 01:03:31 -0600

    fix: Best view with no offset between main and side

--- src/Components/HomePage/MainContent/index.js
+++ src/Components/HomePage/MainContent/index.js
@@ -10,7 +10,7 @@ const MainContent = (props) => (
     <div className='container-fluid'>
       <div className='row'>
         <section className='col-md-offset-1 col-md-7'><Feed /></section>
-        <aside className='col-md-offset-1 col-md-3 visible-md-block visible-lg-block'><SideBar /></aside>
+        <aside className='col-md-3 visible-md-block visible-lg-block'><SideBar /></aside>
       </div>
     </div>
   </div>
